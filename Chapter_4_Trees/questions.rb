f7a09031b828071de0048bc386b362096de8b8a6
Lawrence Ng <ng.Lawrence2@gmail.com>
2018-09-24 11:48:56 -0700

    tree

--- Chapter_4_Trees/questions.rb
+++ Chapter_4_Trees/questions.rb
@@ -48,3 +48,73 @@ def level_order(root)
     end
     res
 end
+
+
+
+#depth of a bst
+def max_depth(root)
+    #iterative
+    # res = 0
+    # return res if root.nil?
+    # current = [root]
+    # until current.empty?
+    #     res+=1
+    #     parents = current
+    #     current = []
+    #     parents.each do |node|
+    #         current << node.left unless node.left.nil?
+    #         current << node.right unless node.right.nil?
+    #     end
+    # end
+    # res
+
+    #recursive
+    return 0 if root.nil?
+    left = max_depth(root.left) + 1
+    right = max_depth(root.right) + 1
+    [left,right].max
+end
+
+
+#4.4
+#Implement a function to check if a binary tree is balanced. For the purposes of this question, a balanced tree is defined to be a tree such that the heights of the two subtrees of any node never differ by more than one.
+def is_balanced(root)
+    return true if root.nil?
+    left_height = depth(root.left)
+    right_height = depth(root.right)
+    result = (left_height - right_height).abs
+    return false if result > 1
+    return is_balanced(root.left) && is_balanced(root.right)
+end
+
+#4.5
+#validate BST
+def is_valid_bst(root)
+    is_sorted(in_order_traversal(root))
+end
+
+def is_sorted(array)
+    return true if array.length == 1
+    prev = 0
+    next_element = 1
+    while next_element < array.length
+        if array[next_element] <= array[prev]
+            return false
+        end
+        next_element += 1
+        prev +=1
+    end
+    true
+end
+
+def in_order_traversal(root,arr=[])
+    return [] if root.nil?
+    unless root.left.nil?
+        in_order_traversal(root.left,arr)
+    end
+    arr << root.val
+    unless root.right.nil?
+         in_order_traversal(root.right,arr)
+    end
+    arr
+end
