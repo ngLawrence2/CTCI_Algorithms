b7200b593cfdf9f9176aa4a3dd83f00197b5d20b
Lawrence Ng <ng.Lawrence2@gmail.com>
2018-10-03 20:51:18 -0700

    finish reverse link list

--- Chapter_2_LinkedList/reverse_linked_list.js
+++ Chapter_2_LinkedList/reverse_linked_list.js
@@ -1,3 +1,32 @@
 const reverse_list = head => {
+  let prev = null;
+  let next = null;
+  while(head!==null) {
+    let next = head.next;
+    head.next = prev;
+    prev = head;
+    head = next;
+  }
+  return prev;
+}
 
+let l3 = {
+  next: null,
+  val: 3
+};
+
+let l2 = {
+  next: l3,
+  val: 2
+};
+let l1 = {
+  next: l2,
+  val: 1
+};
+
+
+let res = reverse_list(l1);
+while(res!==null) {
+  console.log(res.val);
+  res=res.next;
 }
